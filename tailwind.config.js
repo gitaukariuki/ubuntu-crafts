672457706103206b9779220ed58c4495a2597721
gitaukariuki <gitaukariuki@gmail.com>
2025-05-25 16:28:55 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // <--- Make sure this line is correct
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
